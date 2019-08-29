f7bb1ccc771e3a686cc85364b3d93a04091c4f36
Lia <lia.tarar@hackney.gov.uk>
2019-08-29 09:29:15 +0100

    Add test for product list

--- test/ProductAPI.Tests/ProductTests.cs
+++ test/ProductAPI.Tests/ProductTests.cs
@@ -1,17 +1,20 @@
 using NUnit.Framework;
 using ProductAPI.Models;
+using System.Collections.Generic;
 
 namespace ProductAPI.Tests
 {
     public class ProductTests
     {
-        // [SetUp]
-        // public void Setup()
-        // {
-        // }
-
         [Test]
         public void Test1()
+        {
+            var product = new Product();
+            Assert.IsNull(product.Description);
+        }
+
+        [Test]
+        public void Test2()
         {
             // arrange
             var product = new Product{
@@ -23,6 +26,31 @@ namespace ProductAPI.Tests
 
             // act
             var actual = product.Name;
+
+            // assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            // arrange
+            var product_list = new List<Product>{
+                new Product {
+                    ProductID = 1,
+                    Name = "My First Product",
+                    Description = "No description"
+                },
+                new Product {
+                    ProductID = 2,
+                    Name="My Second Product",
+                    Description="No description"
+                }
+            };
+            var expected = "My Second Product";
+
+            // act
+            var actual = product_list[1].Name;
 
             // assert
             Assert.AreEqual(expected,actual);
